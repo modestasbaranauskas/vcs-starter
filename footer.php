271d51ce56cf988f73bc54cee62fcb4a5cf4c9a0
modzius <baranauskas1991@gmail.com>
2019-09-15 22:13:00 +0300

    add copyright and social icons settings

--- footer.php
+++ footer.php
@@ -3,28 +3,24 @@
 	<footer id="footer" class="footer-section">
 		<div class="footer-container flex-container">
 			<div class="copyright-column">
-				<p>Copyright 2014 Focal - Built With Passion</p>
+				<p><?php the_field('f_copyright'); ?> &copy; COPYRIGHT 2014-<?php echo date("Y"); ?></p>
 			</div>
 			<div class="social-media-icon">
 				<ul class="social-media-nav flex-container">
-					<li>
-						<a href="https://twitter.com/m0dzius"><i class="fab fa-twitter fa-fw"></i></a>
-					</li>
-					<li>
-						<a href="https://www.facebook.com/MoBarLT/"><i class="fab fa-facebook-f fa-fw"></i></a>
-					</li>
-					<li>
-						<a href="https://www.instagram.com/mobar_design/"><i class="fab fa-instagram fa-fw"></i></a>
-					</li>
-					<li>
-						<a href="https://www.behance.net/modestabaranau"><i class="fab fa-behance fa-fw"></i></a>
-					</li>
-					<li>
-						<a href="https://www.linkedin.com/"><i class="fab fa-linkedin-in fa-fw"></i></a>
-					</li>
-					<li>
-						<a href="https://github.com/modzius"><i class="fab fa-github fa-fw"></i></a>
-					</li>
+				<?php
+					if(have_rows('f_social_media_repeater')):
+						while(have_rows('f_social_media_repeater')):
+							the_row();
+
+							$link = get_sub_field('link');
+							?>
+							<li>
+								<a href="<?php echo $link['url']; ?>"><i class="<?php the_sub_field('icon'); ?> fa-fw"></i></a>
+							</li>
+							<?php
+						endwhile;
+					endif;
+				?>
 				</ul>
 			</div>
 		</div>
