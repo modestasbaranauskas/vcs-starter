27b83ce2d8c7085919513eecaf6f6a103dc7e5e4
modzius <baranauskas1991@gmail.com>
2019-09-15 15:52:53 +0300

    create navbar section

---
+++ partials/navbar.php
@@ -0,0 +1,33 @@
+<!-- HEADER start
+================================================== -->
+<section id="nav" class="main-nav-section">
+    <div class="main-nav-container flex-container">
+        <div class="main-nav-logo">
+            <a class="js-scroll-trigger flex-logo" href="#hero">
+            <?php
+            $image = get_field('ho_image', 'option');
+            // dump($image);
+            ?>    
+            <img src="<?php echo $image['sizes']['logo']; ?>" alt="<?php echo $image['alt']; ?>">
+            </a>
+        </div>
+        <div class="main-nav-column">
+            <button class="burger"><i class="fas fa-bars"></i></button>
+            <nav id="main-nav" class="main-nav">
+
+            <?php 
+                $menu_options = [
+                    'menu_class' => "nav-menu-list flex-container",
+                    'container' => false,
+                    'theme_location' => "primary-navigation",
+                    'walker' => new custom_navwalker()
+                ];
+
+                wp_nav_menu($menu_options);
+            ?>
+            </nav>
+        </div>
+    </div>
+</section>
+<!-- HEADER end
+================================================== -->
