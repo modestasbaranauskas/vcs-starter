766ccfb65737f161e89de46cec0f5e6c2cae2edb
modzius <baranauskas1991@gmail.com>
2019-09-15 22:12:01 +0300

    create contact section

---
+++ partials/contact.php
@@ -0,0 +1,29 @@
+<!-- CONTACT start
+================================================== -->
+<section id="contact" class="contact-section">
+    <div class="contact-title">
+        <h2><?php the_field('c_heading'); ?></h2>
+    </div>
+    <div class="contact-container flex-container">
+        <div class="contact-column">
+            <div>
+                <?php the_field('c_description'); ?>
+            </div>
+            <div>
+                <?php the_field('c_address') ?>
+            </div>
+            <div>
+                <?php the_field('c_contact_info') ?>
+            </div>
+        </div>
+        <div class="contact-column">
+            <div class="contact-form">
+                <?php
+                    echo do_shortcode(get_field('c_contact_form_shortcode'));
+                ?>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- CONTACT end
+================================================== -->
