18bc5b44a97272787ef473ac8c5baacf999315cb
modzius <baranauskas1991@gmail.com>
2019-09-18 21:56:09 +0300

    change footer settings to "option"

--- footer.php
+++ footer.php
@@ -3,13 +3,13 @@
 	<footer id="footer" class="footer-section">
 		<div class="footer-container flex-container">
 			<div class="copyright-column">
-				<p><?php the_field('f_copyright'); ?> &copy; COPYRIGHT 2014-<?php echo date("Y"); ?></p>
+				<p><?php the_field('fo_copyright', 'option'); ?> &copy; COPYRIGHT 2017-<?php echo date("Y"); ?></p>
 			</div>
 			<div class="social-media-icon">
 				<ul class="social-media-nav flex-container">
 				<?php
-					if(have_rows('f_social_media_repeater')):
-						while(have_rows('f_social_media_repeater')):
+					if(have_rows('fo_social_media_repeater', 'option')):
+						while(have_rows('fo_social_media_repeater', 'option')):
 							the_row();
 
 							$link = get_sub_field('link');
