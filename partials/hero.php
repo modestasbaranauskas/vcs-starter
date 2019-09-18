eb15fbec8e7b7f46c8ed46e3dd5b83bf52048d18
modzius <baranauskas1991@gmail.com>
2019-09-18 21:54:49 +0300

    add repeater to hero.php

--- partials/hero.php
+++ partials/hero.php
@@ -1,22 +1,27 @@
 <!-- HERO start
-    ================================================== -->
-    
-    <?php 
-    
-    $image = get_field('h_background');
-    // dump($image);
-    ?>
-	<section id="hero" class="hero-section" style="background-image: url(<?php echo $image['sizes']['large'] ?>);">
-		<div class="hero-container flex-container">
-			<div class="hero-column">
-                <h1><?php the_field('h_heading') ?></h1>
-                <?php
-                $link = get_field('h_link');
-                // dump($link);
-                ?>
-				<a class="js-scroll-trigger" href="<?php echo $link['url'] ?>"><?php echo $link['title'] ?></a>
-			</div>
-		</div>
-	</section>
-	<!-- HERO end
-    ================================================== -->
+================================================== -->
+<section id="hero" class="hero-section owl-carousel owl-theme">
+<?php
+    if(have_rows('h_hero_section_repeater')):
+        while(have_rows('h_hero_section_repeater')):
+            the_row();
+            $image = get_sub_field('background');
+            // dump($image);
+            $link = get_sub_field('link');
+            // dump($link);
+            ?>
+            <div class="hero-container" style="background-image: url(<?php echo $image['sizes']['hero'] ?>);">
+                <div class="bg-overlay flex-container">
+                    <div class="hero-column">
+                        <h1><?php the_sub_field('heading') ?></h1>
+                        <a class="js-scroll-trigger" href="<?php echo $link['url'] ?>"><?php echo $link['title'] ?></a>
+                    </div>
+                </div>
+            </div>
+            <?php
+        endwhile;
+    endif;
+?>
+</section>
+<!-- HERO end
+================================================== -->
