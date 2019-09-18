6d2dcdfffe9c454d31652542551d3d93e421fa72
modzius <baranauskas1991@gmail.com>
2019-09-18 21:52:41 +0300

    add repeater to summary.php

--- partials/summary.php
+++ partials/summary.php
@@ -1,21 +1,21 @@
 <!-- SUMMARY start
-    ================================================== -->
-	<section class="summary-section">
-		<div id="summary" class="summary-container flex-container">
-        <?php
-            if(have_rows('s_summary_repeater')):
-                while(have_rows('s_summary_repeater')):
-                    the_row();
-                    ?>
-                    <div class="summary-column">
-			            <h2><?php the_sub_field('heading') ?></h2>
-			            <p><?php the_sub_field('description') ?></p>
-		            </div>
-                    <?php
-                endwhile;
-            endif;
-        ?>    
-		</div>
-	</section>
-	<!-- SUMMARY end
-    ================================================== -->
+================================================== -->
+<section class="summary-section">
+    <div id="summary" class="summary-container flex-container">
+    <?php
+        if(have_rows('s_summary_repeater')):
+            while(have_rows('s_summary_repeater')):
+                the_row();
+                ?>
+                <div class="summary-column">
+                    <h2><?php the_sub_field('heading') ?></h2>
+                    <p><?php the_sub_field('description') ?></p>
+                </div>
+                <?php
+            endwhile;
+        endif;
+    ?>    
+    </div>
+</section>
+<!-- SUMMARY end
+================================================== -->
